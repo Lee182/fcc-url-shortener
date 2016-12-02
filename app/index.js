ffc8e2f010c0cb5f6e9441053807f42a6636a092
Jonathan T L Lee <jono-lee@hotmail.co.uk>
2016-12-02 18:52:23 +0000

    styled fancy input box

--- app/index.js
+++ app/index.js
@@ -5,9 +5,26 @@ function insert_borrow_url(url, cb) {
   postJSON({
     url: '/',
     data: {url},
-    cb
+    cb: cb
   })
 }
+$form = document.querySelector('.url_form')
+$input = document.querySelector('.url_input')
+$input.addEventListener('keypress', function(e){
+  setTimeout(function(e){
+    var str = $input.value
+    var valid = url_validation(str)
+    if (str === '') {valid = true}
+    if (valid === false){
+      $form.classList.remove('valid')
+      $form.classList.add('invalid')
+    }
+    if (valid === true){
+      $form.classList.remove('invalid')
+      $form.classList.add('valid')
+    }
+  }, 0)
+})
 
 insert_borrow_url('https://www.whatismyreferer.com/',  function(res){
   console.log(res)
